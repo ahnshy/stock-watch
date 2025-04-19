f8eee6a9e4d0f833ccdce283c1546e68cb5fd81b
ahnshy <ahnshy@naver.com>
2025-04-19 18:16:15 +0900

    <feat> : Create dummy stocks api by get method

---
+++ app/api/stocks/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+const stocks = [
+    { symbol: "005930.KQ", name: "삼성전자", price: 78500 },
+    { symbol: "000660.KQ", name: "SK하이닉스", price: 134000 },
+    { symbol: "035420.KQ", name: "NAVER", price: 212000 },
+];
+
+export async function GET() {
+    return NextResponse.json(stocks);
+}
