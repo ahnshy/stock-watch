f1ca5ef8f9dd124d89e251a98486dae3c4f458cc
ahnshy <ahnshy@naver.com>
2025-04-20 20:42:08 +0900

    <feat> : Adding a FIN HUB API key

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        FINNHUB_API_KEY: process.env.FINNHUB_API_KEY,
+    },
 };
 
 export default nextConfig;
